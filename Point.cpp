29baae824c4e89b746a42cd8b090e1acef2305d4
Hafeez <izzhafeez@gmail.com>
2022-08-31 17:20:13 +0800

    Edited ./Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -4,11 +4,15 @@ class Point {
   public:
     float x;
     float y;
+
+    Point(float x, float y) {
+      x = x;
+      y = y;
+    }
     
     float distanceTo(Point *point) {
       return pow(
-        pow(x - point->x, 2) + pow(y - point->y, 2),
-        0.5
+        pow(x - point->x, 2) + pow(y - point->y, 2), 0.5
         );
     }
 };
